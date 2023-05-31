64a7ea9a031e74e2b606c570afa0e40c25002edb
Khà Khà Khà <khakhakha@MacBook-Pro-cua-GALAFUR.local>
2023-05-31 20:55:59 +0700

    API Get Product By Category ID

--- src/main/java/com/cybersoft/cozastore/controller/ProductController.java
+++ src/main/java/com/cybersoft/cozastore/controller/ProductController.java
@@ -1,6 +1,9 @@
 package com.cybersoft.cozastore.controller;
 
 import com.cybersoft.cozastore.payload.response.BaseResponse;
+import com.cybersoft.cozastore.repository.ProductRepository;
+import com.cybersoft.cozastore.service.imp.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+    @Autowired
+    private IProductService iProductService;
     @GetMapping("/{id}")
-    public ResponseEntity<?> getProductByCategory(){
+    public ResponseEntity<?> getProductByCategory(int id){
         BaseResponse response = new BaseResponse();
-
+        response.setData(iProductService.getProductByCategoryId(id));
         return new ResponseEntity<>("", HttpStatus.OK);
     }
 }
